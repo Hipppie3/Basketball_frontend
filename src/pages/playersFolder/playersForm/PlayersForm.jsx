a6abeecedcaa845aaff5eb488aae9c5346b36e52
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-03 19:57:14 -0700

    test

--- src/pages/playersFolder/playersForm/PlayersForm.jsx
+++ src/pages/playersFolder/playersForm/PlayersForm.jsx
@@ -1,78 +1,18 @@
-import React, { useState } from 'react'
-import './AddNewPlayer.css'
-import axios from 'axios';
+import React from 'react'
+import './PlayersForm.css'
+import AddNewPlayers from './AddNewPlayers';
+// import AddStatsPage from './AddStatsPage';
+
 
 function PlayersForm() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-
-
-
-  const handlePlayerSubmit = async (e) => {
-  e.preventDefault();
-    console.log('Hi')
-  try {
-    const response = await axios.post('https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players', {
-      player: {
-        first_name: firstName,
-        last_name: lastName,
-      },
-    });
-    console.log(response.data);
-    // Reset form values
-    setFirstName('');
-    setLastName('');
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-
-
+  
   return (
-   <div className="addNewPlayerContainer">
-    <h1 className="addPlayerTitle">Add New Player</h1>
-
-      <form onSubmit={handlePlayerSubmit} className="newPlayer">
-        <label className="newPlayerLabel">
-          First Name:
-          <input
-            className="newPlayerInput"
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label className="newPlayerLabel">
-          Last Name:
-          <input
-            className="newPlayerInput"
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <button className="addPlayerBtn" type="submit">Add Player</button>
-      </form>
-
-
-
-
-        <h1 className="deletePlayerTitle">Delete Player</h1>
-      <form /*onSubmit={handleDeletePlayer}*/ className="deletePlayer">
-        <label>
-          Player ID:
-          <input
-            className="deletePlayerInput"
-            type="number"
-            // value={deletePlayerId}
-            // onChange={(e) => setDeletePlayerId(e.target.value)}
-          />
-        </label>
-        <button type="submit" className="deletePlayerBtn">Delete Player</button>
-      </form>
+    <div className="playersform-page">
+      <div>
+      <AddNewPlayers/>
+      </div>
+      {/* <AddStatsPage/> */}
     </div>
-
   )
 }
 
