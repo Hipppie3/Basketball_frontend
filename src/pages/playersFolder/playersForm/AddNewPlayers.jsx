760bf05afadc9a4d798a22c24f2e76ddcee01599
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-05 13:55:26 -0700

    test

--- src/pages/playersFolder/playersForm/AddNewPlayers.jsx
+++ src/pages/playersFolder/playersForm/AddNewPlayers.jsx
@@ -30,15 +30,22 @@ function AddNewPlayers() {
 };
 
 
-  const handleDeletePlayer = async () => {
-    try {
-      await axios.delete(`https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players/${deletePlayerId}`);
-      // Reset form values
-      setDeletePlayerId('');
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const handleDeletePlayer = async () => {
+  try {
+    await axios.delete(`https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players/${deletePlayerId}`, {
+      data: {
+        player: {
+          id: deletePlayerId,
+        },
+      },
+    });
+    // Reset form values
+    setDeletePlayerId('');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
 
   return (
