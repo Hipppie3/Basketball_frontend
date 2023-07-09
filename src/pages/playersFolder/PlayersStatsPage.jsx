1da3ae519e681293ef0a3c6b8a138b94be3bd897
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-08 20:14:58 -0700

    test

--- src/pages/playersFolder/PlayersStatsPage.jsx
+++ src/pages/playersFolder/PlayersStatsPage.jsx
@@ -12,7 +12,7 @@ function PlayersStatsPage() {
       try {
         const response = await axios.get(`https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players/${id}`);
         setPlayer(response.data);
-        console.log(response.data.image_url)
+        console.log(response.data)
       } catch (error) {
         console.error('Something went wrong:', error);
       }
