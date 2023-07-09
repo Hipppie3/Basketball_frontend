4648e1462c11a2a36880c958451f7e461063cfb5
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-08 19:59:50 -0700

    test

--- src/pages/playersFolder/Players.jsx
+++ src/pages/playersFolder/Players.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import './Players.css';
-import { FaSearch } from 'react-icons/fa'
-
-
+import { FaSearch } from 'react-icons/fa';
 
 function Players() {
   const [players, setPlayers] = useState(null);
@@ -15,7 +13,7 @@ function Players() {
       try {
         const response = await axios.get('https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players');
         setPlayers(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (error) {
         console.error('Something went wrong:', error);
       }
@@ -35,33 +33,31 @@ function Players() {
 
   return (
     <div className="player-container">
-
       <div className="player-card-container">
-        <h1 className='sports-title'>PLAYERS</h1>
-      <div className="search-input">
-        <input type="text" placeholder="Search for a player" />
-        <button type="submit">
-        <FaSearch />
-        </button>
-      </div>
-
-        
-        <div className="player-name-container">
-        {players.map((player) => (
-          <div className="player-card">
-            <div key={player.id} >
-            <Link className="player-name" to={`/players/${player.id}`} onClick={(event) => handlePlayerClick(event, player.id)}>
-          <img src={player.image_url} className="player-card-image"/>
-              {player.first_name} {player.last_name} {player.id}
-            </Link>
-          </div>
-          <div className="player-sports">BASKETBALL</div>
-          </div>
-        ))}
+        <h1 className="sports-title">PLAYERS</h1>
+        <div className="search-input">
+          <input type="text" placeholder="Search for a player" />
+          <button type="submit">
+            <FaSearch />
+          </button>
         </div>
 
+        <div className="player-name-container">
+          {players.map((player) => (
+            <div className="player-card" key={player.id}>
+              <Link
+                className="player-name"
+                to={`/players/${player.id}`}
+                onClick={(event) => handlePlayerClick(event, player.id)}
+              >
+                <img src={player.image_url} className="player-card-image" />
+                {player.first_name} {player.last_name} {player.id}
+              </Link>
+              <div className="player-sports">BASKETBALL</div>
+            </div>
+          ))}
+        </div>
       </div>
-
     </div>
   );
 }
