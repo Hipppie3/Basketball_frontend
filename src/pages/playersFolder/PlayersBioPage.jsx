e2f81467a40207bce6fea4097b2e215ded69a021
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-09 18:22:13 -0700

    test

--- src/pages/playersFolder/PlayersBioPage.jsx
+++ src/pages/playersFolder/PlayersBioPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink, useParams } from 'react-router-dom'
 
 function PlayersBioPage() {
+  const { id } = useParams();
+  const [player, setPlayer] = useState(null);
+
+   useEffect(() => {
+    const fetchPlayerData = async () => {
+      try {
+        const response = await axios.get(`https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players/${id}`);
+        setPlayer(response.data);
+      } catch (error) {
+        console.error('Something went wrong:', error);
+      }
+    };
+
+    fetchPlayerData();
+  }, [id]);
   return (
     <div>
-      Bio
+      
+          <div className="players-link-left1">     
+            <ul>
+              <li>
+              <NavLink className='stats-link'to={`/players/${player.player.id}`}>Profile</NavLink></li>
+              <li>Stats</li>
+              <li><NavLink to={`/players/${player.player.id}/bio`} className='stats-link' >Bio</NavLink></li>
+
+              <li><NavLink to={`/players/${player.player.id}/media`} className='stats-link' >Media</NavLink></li>
+            </ul>
+          </div>
+   
+        
     </div>
   )
 }
--- src/pages/playersFolder/PlayersMediaPage.jsx
+++ src/pages/playersFolder/PlayersMediaPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function PlayersMediaPage() {
   return (
     <div>
-      Players Media
+              <div className="players-link-left1">
+            
+            <ul>
+              <li>
+              <NavLink className='stats-link'to={`/players/${player.player.id}`}>Profile</NavLink></li>
+              <li>Stats</li>
+              <li><NavLink to={`/players/${player.player.id}/bio`} className='stats-link' >Bio</NavLink></li>
+
+              <li><NavLink to={`/players/${player.player.id}/media`} className='stats-link' >Media</NavLink></li>
+            </ul>
+          </div>
+   
     </div>
   )
 }
