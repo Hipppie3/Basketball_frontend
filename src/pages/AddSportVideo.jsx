0c707e0946e8938e41a2a1eb7536571eaccc79c4
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 17:30:54 -0700

    test

--- src/pages/AddSportVideo.jsx
+++ src/pages/AddSportVideo.jsx
@@ -31,7 +31,7 @@ const handleAddSportMediaVideo = async () => {
   try {
     const response = await axios.post(
       `https://agile-reef-32463-2ad3559c3e00.herokuapp.com/sports/${selectedSport}/sport_media_videos`,
-      { video: videoUrl, title: 'Some Title' }
+      { video: { url: videoUrl, title: 'Some Title' } }
     );
     console.log(response.data);
     setSelectedSport('');
@@ -40,6 +40,7 @@ const handleAddSportMediaVideo = async () => {
     console.log(error);
   }
 };
+
 
 
   return (
