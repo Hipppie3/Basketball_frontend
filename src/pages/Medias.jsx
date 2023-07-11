dcf7d390efa473b33ccf13b8b8636d1644a208e3
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-10 17:49:37 -0700

    test

--- src/pages/Medias.jsx
+++ src/pages/Medias.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import YouTube from 'react-youtube';
 import './Medias.css';
 
 function Medias() {
@@ -14,7 +15,7 @@ function Medias() {
     try {
       const response = await axios.get('https://agile-reef-32463-2ad3559c3e00.herokuapp.com/sports');
       setSports(response.data);
-      console.log(response.data)
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -24,25 +25,36 @@ function Medias() {
     setActiveCategory(category);
   };
 
-const renderSportMediaVideos = (sport) => {
-  if (activeCategory === 'all' || activeCategory === sport.name.toLowerCase()) {
-    if (sport.sport_media_videos && sport.sport_media_videos.length > 0) {
-      return sport.sport_media_videos.map((video) => (
-        <article key={video.id} className="media-container">
-          <a href={video.url} className="thumbnail" data-duration="12:24">
-            <img className="thumbnail-image" src="http://unsplash.it/250/150?gravity=center" alt="Thumbnail" />
-          </a>
-          <div className="media-bottom-section">
-            <div className="media-details">
-              <a href={video.url} className="media-title">{video.title}</a>
-              <a href={video.url} className="media-description">Media Description</a>
+  const renderSportMediaVideos = (sport) => {
+    if (activeCategory === 'all' || activeCategory === sport.name.toLowerCase()) {
+      if (sport.sport_media_videos && sport.sport_media_videos.length > 0) {
+        return sport.sport_media_videos.map((video) => (
+          <article key={video.id} className="media-container">
+            <YouTube videoId={getVideoId(video.url)} className="thumbnail" data-duration="12:24" />
+            <div className="media-bottom-section">
+              <div className="media-details">
+                <a href={video.url} className="media-title">
+                  {video.title}
+                </a>
+                <a href={video.url} className="media-description">
+                  Media Description
+                </a>
+              </div>
             </div>
-          </div>
-        </article>
-      ));
+          </article>
+        ));
+      }
     }
-   }
     return null;
+  };
+
+  const getVideoId = (url) => {
+    const match = url.match(/youtube\.com.*(\?v=|\/embed\/|\/\d\/|\/vi\/|\/v\/|https:\/\/youtu\.be\/|\/e\/|watch\?v=|\&v=|youtu\.be\/|\/v\/|e\/|youtube\.com\/v\/)([^#\&\?\n]*)/);
+    if (match && match[2].length === 11) {
+      return match[2];
+    } else {
+      return null;
+    }
   };
 
   return (
@@ -72,24 +84,21 @@ const renderSportMediaVideos = (sport) => {
             {sports.map((sport) => (
               <section key={sport.id} className="media-section">
                 <h2 className="media-section-title">{sport.name}</h2>
-                <div className="media-container">
-                  {renderSportMediaVideos(sport)}
-                </div>
+                <div className="media-container">{renderSportMediaVideos(sport)}</div>
               </section>
             ))}
           </>
         ) : (
           <>
-            {sports.map((sport) => (
-              activeCategory === sport.name.toLowerCase() && (
-                <section key={sport.id} className="media-section">
-                  <h2 className="media-section-title">{sport.name}</h2>
-                  <div className="media-container">
-                    {renderSportMediaVideos(sport)}
-                  </div>
-                </section>
-              )
-            ))}
+            {sports.map(
+              (sport) =>
+                activeCategory === sport.name.toLowerCase() && (
+                  <section key={sport.id} className="media-section">
+                    <h2 className="media-section-title">{sport.name}</h2>
+                    <div className="media-container">{renderSportMediaVideos(sport)}</div>
+                  </section>
+                )
+            )}
           </>
         )}
       </div>
