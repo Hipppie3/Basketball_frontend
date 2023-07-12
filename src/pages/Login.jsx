7e07b240f3be99b3a7dd7d55fb94d2030a619700
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-11 19:10:17 -0700

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
-  const [username, setUsername] = useState('');
+  const [username, setName] = useState('');
   const [password, setPassword] = useState('');
+  const { setLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleNameChange = (event) => {
+    setName(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
@@ -19,16 +21,19 @@ function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    // Make a request to your Rails backend for authentication
+    // Replace 'your_backend_endpoint' with the actual endpoint URL
     axios
       .post('https://agile-reef-32463-2ad3559c3e00.herokuapp.com/login', {
         username,
         password
       })
       .then((response) => {
-        const { token } = response.data;
-        if (token) {
-          localStorage.setItem('token', token);
-          navigate('/');
+        const user = response.data;
+        if (response.status === 201) {
+          setLoggedIn(true);
+          console.log(user); // Use console.log to display the user data
+           navigate('/'); // Navigate to the desired route after successful login
         } else {
           console.error('Login failed');
         }
@@ -44,12 +49,12 @@ function Login() {
         <h1 className="loginTitle">Log In</h1>
         <input
           className="input"
-          placeholder="Enter Username"
+          placeholder="Enter Name"
           type="text"
           id="username"
           autoComplete="off"
           value={username}
-          onChange={handleUsernameChange}
+          onChange={handleNameChange}
         />
         <input
           className="input"
