438ae65c49f489bdc808c000f40b0e15c79c8d14
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-07-12 15:58:38 -0700

    test

--- src/pages/playersFolder/Players.jsx
+++ src/pages/playersFolder/Players.jsx
@@ -4,18 +4,19 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Players.css';
 import { FaSearch } from 'react-icons/fa'
 
-
-
 function Players() {
   const [players, setPlayers] = useState(null);
+  const [sortedPlayers, setSortedPlayers] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
   const navigate = useNavigate();
- 
+
   useEffect(() => {
     const fetchPlayerData = async () => {
       try {
         const response = await axios.get('https://agile-reef-32463-2ad3559c3e00.herokuapp.com/players');
         setPlayers(response.data);
-        console.log(response.data)
+        setSortedPlayers([...response.data].sort((a, b) => a.first_name.localeCompare(b.first_name)));
       } catch (error) {
         console.error('Something went wrong:', error);
       }
@@ -24,7 +25,23 @@ function Players() {
     fetchPlayerData();
   }, []);
 
-  if (!players) {
+  useEffect(() => {
+    filterPlayers();
+  }, [sortedPlayers, searchQuery]);
+
+  const filterPlayers = () => {
+    if (!searchQuery) {
+      setFilteredPlayers(sortedPlayers);
+    } else {
+      const filtered = sortedPlayers.filter(
+        (player) =>
+          player.first_name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredPlayers(filtered);
+    }
+  };
+
+  if (!filteredPlayers) {
     return <div>Loading...</div>;
   }
 
@@ -35,33 +52,41 @@ function Players() {
 
   return (
     <div className="player-container">
-
       <div className="player-card-container">
         <h1 className='sports-title'>PLAYERS</h1>
-      <div className="search-input">
-        <input type="text" placeholder="Search for a player" />
-        <button type="submit">
-        <FaSearch />
-        </button>
-      </div>
-
-        
-        <div className="player-name-container">
-        {players.map((player) => (
-          <div className="player-card" key={player.id} >
-            <div>
-            <Link className="player-name" to={`/players/${player.id}`} onClick={(event) => handlePlayerClick(event, player.id)}>
-            <img src={player.image_url} className="player-card-image"/>
-              {player.first_name} {player.last_name} {player.id}
-            </Link>
-          </div>
-          <div className="player-sports">Basketball</div>
-          </div>
-        ))}
+        <div className="search-input">
+          <input
+            type="text"
+            placeholder="Search for a player"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button type="submit">
+            <FaSearch />
+          </button>
         </div>
-
+        <div className="player-name-container">
+          {filteredPlayers.map((player) => (
+            <div className="player-card" key={player.id}>
+              <div>
+                <Link
+                  className="player-name"
+                  to={`/players/${player.id}`}
+                  onClick={(event) => handlePlayerClick(event, player.id)}
+                >
+                  <img
+                    src={player.image_url}
+                    className="player-card-image"
+                    alt="Player"
+                  />
+                  {player.first_name} {player.last_name} {player.id}
+                </Link>
+              </div>
+              <div className="player-sports">Basketball</div>
+            </div>
+          ))}
+        </div>
       </div>
-
     </div>
   );
 }
